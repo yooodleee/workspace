78dcffe48850992dc35aa029feb545793153a4f8
yooodleee <accia25@naver.com>
2025-05-22 21:45:01 +0900

    terraform fmt

--- start/main.tf
+++ start/main.tf
@@ -1,9 +1,9 @@
 resource "local_file" "abc" {
-content = "abc!"
-filename = "${path.module}/abc.txt"
+  content  = "abc!"
+  filename = "${path.module}/abc.txt"
 }
 
 resource "local_file" "def" {
-content = "def!"
-filename = "#{path.module}/def.txt"
+  content  = "def!"
+  filename = "#{path.module}/def.txt"
 }
