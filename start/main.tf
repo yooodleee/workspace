9d04db76b91b829a8aa299f07433f2c14637a695
yooodleee <accia25@naver.com>
2025-05-28 16:55:33 +0900

    overrating required providers version

--- start/main.tf
+++ start/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.0.0"
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = ">= 10000.0.0"
+    }
+  }
 }
 
 resource "local_file" "abc" {
