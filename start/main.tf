5884b09d9b1ca7024e0d61ece9e98b2130c614c4
yooodleee <accia25@naver.com>
2025-05-28 18:01:14 +0900

    code

--- start/main.tf
+++ start/main.tf
@@ -13,9 +13,9 @@ terraform {
       name = "my-app-prod"
     }
   }
-  backend "local" {
-    path = "state/terraform.tfstate"
-  }
+  # backend "local" {
+  #   path = "state/terraform.tfstate"
+  # }
 }
 
 resource "local_file" "abc" {
